39a968397581d268300eb669d1363b599755b664
seanb-github <93153329+seanb-github@users.noreply.github.com>
2024-01-23 17:30:44 +0100

    Add files via upload

---
+++ shap_plots.R
@@ -0,0 +1,62 @@
+library(pheatmap) ## for heatmap generation
+library(tidyverse) ## for data wrangling
+library(ggplotify) ## to convert pheatmap to ggplot2
+library(heatmaply) ## for constructing interactive heatmap
+
+setwd("C:/Users/sreabagcik/git/multiverse_cluster_analysis/Data")
+
+rownames(shap_s2) <- shap_s2[,1]
+
+shap_s2 <- shap_s2[,-1]
+
+shap_s2 <- t(shap_s2)
+
+rownames(shap_s2) <- c('Age', 'GCS', 'GCS Motor', 'Imaging PCA 1', 'Cause = Fall', 'Imaging PCA 3',
+                       'Imaging PCA 2', 'Imaging PCA 4', 'Cause = Traffic', 'Major Extracranial Injury',
+                       'Cause = Other', 'Cause = Violence', 'Pupils', 'Hypotension', 'Hypoxia')
+
+#
+
+saveRDS(shap_s2, file="shap_s2_v2.rds")
+
+#
+
+shap_s2 <- readRDS("shap_s2_v2.rds")
+
+shap_s2 <- prop.table(shap_s2, margin = 1)
+
+#
+
+shap_s1 <- readRDS("shap_s1.rds")
+
+rownames(shap_s1) <- c('AC-Gow-Sil', 'KM-Gow-Sil', 'SC-Gow-Sil','AC-Eucl-Sil', 'KM-Eucl-Sil', 'SC-Eucl-Sil',
+                       'AC-Gow-Gap', 'KM-Gow-Gap', 'SC-Gow-Gap', 'AC-Eucl-Gap', 'KM-Eucl-Gap', 'SC-Eucl-Gap')
+
+shap_s1 <- prop.table(shap_s1, margin = 1)
+
+colnames(shap_s1) <- c('Imaging PCA 4', 'Imaging PCA 3', 'Imaging PCA 2', 'Age', 'Imaging PCA 1',
+                       'Major Extacranial Injury', 'GCS', 'Hypotension', 'GCS Motor', 'Cause = Violence',
+                       'Cause = Traffic', 'Pupils', 'Cause = Fall',  'Hypoxia', 'Cause = Other')
+pheatmap(shap_s1,
+         clustering_method = "complete",
+         col = colorRampPalette(c("white", "grey50", "black"))(100),
+         clustering_distance_rows = "manhattan",
+         clustering_distance_columns = "correlation",
+         scale = "none",
+         main = "",
+         annotation_col = NULL,
+         angle_col=45,
+         legend=FALSE)
+
+#
+
+pheatmap(shap_s2,
+         clustering_method = "complete",
+         col = colorRampPalette(c("white", "grey50", "black"))(100),
+         clustering_distance_rows = "manhattan",
+         scale = "none",
+         distfun = "correlation",
+         main = "",
+         annotation_col = NULL,
+         angle_col=45,
+         legend=FALSE)
---
+++ upset_plots.R
@@ -0,0 +1,88 @@
+cat("\014")
+rm(list=ls())
+
+{
+  library(ComplexUpset)
+  library(ggplot2)
+}
+
+#
+M2_BinaryMatrix <- readRDS("/Users/seanvanbork/Desktop/multiverse_cluster_analysis/Data/M2_BinaryMatrix.rds")
+M2_BinaryMatrix <- M2_BinaryMatrix[, -c(4,6,8)]
+M1_BinaryMatrix <- M1_BinaryMatrix_v2[, -c(3,9)]
+#
+
+set_size = function(w, h, factor=1.5) {
+  s = 1 * factor
+  options(
+    repr.plot.width=w * s,
+    repr.plot.height=h * s,
+    repr.plot.res=100 / factor,
+    jupyter.plot_mimetypes='image/png',
+    jupyter.plot_scale=1
+  )
+}
+#
+
+#
+set_size(8, 3)
+#
+
+upset(
+  head(M2_BinaryMatrix, 10163286), colnames(M2_BinaryMatrix),
+  n_intersections=5,
+  min_degree = 2,
+  sort_sets='descending',
+  sort_intersections='descending',
+  #
+  set_sizes=(
+    upset_set_size(position='right', geom = geom_bar(width = .9))
+    + geom_text(aes(label=paste0(round((..count../nrow(M2_BinaryMatrix))*100))), hjust=1.2, stat='count', color='white')
+    + expand_limits(y=1000)
+    + ylab("")
+    + theme(axis.text.x = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank()
+    )
+  ),
+  #
+  base_annotations=list(
+        'Intersection size'=intersection_size(text_mapping=aes(label=paste0(round(
+          !!get_size_mode('exclusive_intersection')/nrow(M2_BinaryMatrix) * 100
+        ), '%')))
+      + ylab("")
+      + theme(axis.text.y = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank()
+      ),
+        'Intersection ratio'=intersection_ratio(text_mapping=aes(label=!!upset_text_percentage()))
+    + ylab("")
+    + theme(axis.text.y = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank()
+    )
+  ),
+  #
+  height_ratio=1.35,
+  width_ratio=0.3,
+  name="",
+  wrap=TRUE,
+  stripes='white', # c(alpha('grey90', 0.45), alpha('white', 0.3)),
+  matrix = intersection_matrix(    
+    geom=geom_point(
+      shape='square',
+      size=4
+    ),
+    segment=geom_segment(
+      linetype='solid'
+    ), 
+    outline_color=list(
+      active='darkorange3',
+      inactive='grey70'
+    )
+  )
+  #
+) + labs(title = "", caption = "")
